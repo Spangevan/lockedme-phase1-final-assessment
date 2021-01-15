ee6cb8bc70032a0f0a01f17613c4cde15420997d
Spangevan <ewlson23@gmail.com>
2021-01-15 12:34:06 -0500

    designed and implemented cmd welcome screen

--- lockedme-phase1-final-project/src/main/java/com/hcl/App.java
+++ lockedme-phase1-final-project/src/main/java/com/hcl/App.java
@@ -1,13 +1,44 @@
 package com.hcl;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	
+	final static String DIRECTORY = "/";
+	
+	static Scanner scanner = new Scanner(System.in);
+	
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        displayWelcomeScreen();
+    }
+    
+    private static void displayWelcomeScreen() {
+    	System.out.println("~||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~||~");
+    	System.out.println("+||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~||+");
+    	System.out.println("~|| \\             //   _____             ____     ____                   ____     ||~");
+    	System.out.println("+||  \\    //\\    //  ||        ||       |        |    |    //\\    //\\   |         ||+");
+    	System.out.println("~||   \\  //  \\  //   ||=====   ||       |        |    |   //  \\  //  \\  |====     ||~");
+    	System.out.println("+||    \\//    \\//    ||_____   ||____   |____    |____|  //    \\//    \\ |____     ||+");
+    	System.out.println("~||                                                                               ||~");
+    	System.out.println("+||                                                                               ||+");
+    	System.out.println("~||                                                                               ||~");
+    	System.out.println("+||                                                                               ||+");
+    	System.out.println("~||                                                                               ||~");
+    	System.out.println("+||Application: LockedMe.com Phase 1 Final Assessment                             ||+");
+    	System.out.println("~||                                                                               ||~");
+    	System.out.println("+||Developer: Evan Spangenberg                                                    ||+");
+    	System.out.println("~||                                                                               ||~");
+    	System.out.println("+||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~||+");
+    	System.out.println("~||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~||~");
+
+
+
     }
 }
